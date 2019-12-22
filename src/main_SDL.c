b7a31610cf09e534ca61027798fc5a3b25afdc10
Titouan lebocq <lebocq.tit@gmail.com>
2019-12-22 16:22:52 +0100

    commit sdl

--- src/main_SDL.c
+++ src/main_SDL.c
@@ -3,19 +3,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
 //gcc src/main_SDL.c -IC:\MinGW_lib\i686-w64-mingw32\include -IInclude -LC:\MinGW_lib\i686-w64-mingw32\lib -Llib -w -Wl,-subsystem,windows -lmingw32 -lSDL2main -lSDL2 -o test
 int main(int argc, char **argv)
 {
-    /* Initialisation simple */
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)
+    //The window we'll be rendering to
+    SDL_Window *window = NULL;
+
+    //The surface contained by the window
+    SDL_Surface *screenSurface = NULL;
+
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-        fprintf(stdout, "Échec de l'initialisation de la SDL (%s)\n", SDL_GetError());
-        return -1;
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
     }
 
+    else
+    {
+        //Create window
+        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (window == NULL)
+        {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        }
+        else
+        {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface(window);
 
+            //Fill the surface white
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
 
-    /* Quit SDL */
+            //Update the surface
+            SDL_UpdateWindowSurface(window);
+
+            //Wait two seconds
+            SDL_Delay(2000);
+        }
+    }
+    //Destroy window
+    SDL_DestroyWindow(window);
+
+    //Quit SDL subsystems
     SDL_Quit();
 
     return 0;
